c907bf5a77e0fe3d77834e89fad270c9bd3c9d7d
andrejrakic <andrej.rakic@mvpworkshop.co>
2021-10-10 15:08:09 +0200

    Adjust OpenZeppelin import

--- contracts/DeployerV2.sol
+++ contracts/DeployerV2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
+import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
 import { Greeter } from "./Greeter/Greeter.sol";
 
 contract DeployerV2 {
